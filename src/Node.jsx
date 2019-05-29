ae0c06ef3fd6bf36d66ad2b9149a2174dc3307ef
Chris Reeve <paradigmsareconstructed@gmail.com>
2019-05-29 09:37:27 -0700

    Switched Node and TreeRootNode components from functional to class-based components

--- src/Node.jsx
+++ src/Node.jsx
@@ -1,35 +1,53 @@
-import React, { useRef } from 'react';
+// In order to add + remove nodes, we have to generate events w onClick props
+// on the nodes as well as handlers to process those onClick events which have
+// occurred downstream.  Then, in order to actually remove a node, we have to
+// simply apply a conditional inside of the node's parent to control the display.
 
-const Node = props => {
-	const
-		ref = useRef(null),
-		propertiesArray = Object.keys(props.properties),
-		nodeProperties = props.properties,
-		childrenExist = React.Children.count(props.children) > 0;
+// The functionality to process this could optionally be placed inside of
+// the root, but there is no real advantage to doing this since the root would then
+// have to remotely control that parent node's state.  What this suggests is that
+// we need to switch our nodes from functional to class-based components, so that
+// we can implement our click handlers within the parent nodes themselves.
 
-	console.log('props:');
-	console.log(props);
+import React, { Component } from 'react';
 
-	return (
-		<div ref={ref}>
-			<h1>Node</h1>
-			<ul>
-				{ propertiesArray.map(property =>
-					<li key={property}>{`${property}: ${nodeProperties[property]}`}</li>) }
-			</ul>
+class Node extends Component {
+	constructor(props) {
+		super(props);
 
-			{ childrenExist &&
-				<div>
-					<h3>Children:</h3>
-					<ul>
-						{ React.Children.map(props.children, (child, index) =>
-							<li key={`${nodeProperties.name}-${index}`}>
-								{ React.cloneElement(child, {parent: ref}) }
-							</li>) }
-					</ul>
-				</div> }
-		</div>
-	);
+		this.props = props;		
+	}
+
+	render = () => {
+		const
+			propertiesArray = Object.keys(this.props.properties),
+			nodeProperties = this.props.properties,
+			childrenExist = React.Children.count(this.props.children) > 0;
+
+		console.log('this.props:');
+		console.log(this.props);
+
+		return (
+			<div>
+				<h1>Node</h1>
+				<ul>
+					{ propertiesArray.map(property =>
+						<li key={property}>{`${property}: ${nodeProperties[property]}`}</li>) }
+				</ul>
+
+				{ childrenExist &&
+					<div>
+						<h3>Children:</h3>
+						<ul>
+							{ React.Children.map(this.props.children, (child, index) =>
+								<li key={`${nodeProperties.name}-${index}`}>
+									{child}
+								</li>) }
+						</ul>
+					</div> }
+			</div>
+		);
+	}
 };
 
 export default Node;
--- src/TreeRootNode.jsx
+++ src/TreeRootNode.jsx
@@ -1,32 +1,39 @@
-import React, { useRef } from 'react';
+import React, { Component } from 'react';
 
-const TreeRootNode = props => {
-	const
-		ref = useRef(null),
-		propertiesArray = Object.keys(props.properties),
-		childrenExist = React.Children.count(props.children) > 0,
-		nodeProperties = props.properties;
+class TreeRootNode extends Component {
+	constructor(props) {
+		super(props);
 
-	return (
-		<div ref={ref}>
-			<h1>Tree Root</h1>
-			<ul>
-				{ propertiesArray.map(property =>
-					<li key={property}>{`${property}: ${nodeProperties[property]}`}</li>) }
-			</ul>
+		this.props = props;
+	}
 
-			{ childrenExist &&
-				<div>
-					<h3>Children:</h3>
-					<ul>
-						{ React.Children.map(props.children, (child, index) =>
-							<li key={`${nodeProperties.name}-${index}`}>
-								{ React.cloneElement(child, {parent: ref}) }
-							</li>) }
-					</ul>
-				</div> }
-		</div>
-	);
+	render = () => {
+		const
+			propertiesArray = Object.keys(this.props.properties),
+			childrenExist = React.Children.count(this.props.children) > 0,
+			nodeProperties = this.props.properties;
+
+		return (
+			<div>
+				<h1>Tree Root</h1>
+				<ul>
+					{ propertiesArray.map(property =>
+						<li key={property}>{`${property}: ${nodeProperties[property]}`}</li>) }
+				</ul>
+
+				{ childrenExist &&
+					<div>
+						<h3>Children:</h3>
+						<ul>
+							{ React.Children.map(this.props.children, (child, index) =>
+								<li key={`${nodeProperties.name}-${index}`}>
+									{ child }
+								</li>) }
+						</ul>
+					</div> }
+			</div>
+		);
+	}
 };
 
 export default TreeRootNode;
